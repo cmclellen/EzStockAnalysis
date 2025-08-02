e81dcd09da87524a39a868de463ad978ee9f5ced
Craig McLellen <cmclellen@gmail.com>
2025-08-02 08:30:28 +0800

    Fix graph

--- UI/app/_components/StockChart.tsx
+++ UI/app/_components/StockChart.tsx
@@ -14,51 +14,8 @@ import {
   YAxis,
 } from "recharts";
 import { Stock } from "../_lib/types";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import { useEffect, useState } from "react";
+import { getYearToDate } from "../_lib/actions";
 
 function StockPill({ stock, guestId }: { stock: Stock; guestId: number }) {
   const appDispatch = useAppDispatch();
@@ -107,6 +64,21 @@ type StockChartProps = {
 
 function StockChart({ guestId }: StockChartProps) {
   const stockTickers = useAppSelector(getStockTickers);
+  const [serverData, setServerData] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await getYearToDate();
+        setServerData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
     <>
       <StockPillList stocks={stockTickers} guestId={guestId} />
@@ -114,7 +86,7 @@ function StockChart({ guestId }: StockChartProps) {
         <LineChart
           width={500}
           height={300}
-          data={data}
+          data={serverData}
           margin={{
             top: 5,
             right: 30,
@@ -127,12 +99,12 @@ function StockChart({ guestId }: StockChartProps) {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line
+          {/* <Line
             type="monotone"
             dataKey="pv"
             stroke="#8884d8"
             activeDot={{ r: 8 }}
-          />
+          /> */}
           <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
         </LineChart>
       </ResponsiveContainer>
